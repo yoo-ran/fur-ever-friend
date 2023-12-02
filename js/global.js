5dda8364553a05cc3148090382cf603a872f6ebe
YooranKim <yuranm@naver.com>
2023-12-02 01:14:23 -0800

    organize

--- js/global.js
+++ js/global.js
@@ -1,10 +1,11 @@
 
+
     const menuTrigger = document.querySelector('.menu-trigger');
     const dropdownMenu = document.querySelector(".dropdownMenu")
     menuTrigger.addEventListener('click', (event) => {
       event.currentTarget.classList.toggle('active-1');
       $(dropdownMenu).slideToggle()
+
+      
     });
 
-
-$('.main-carousel').flickity();
