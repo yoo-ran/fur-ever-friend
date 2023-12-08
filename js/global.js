cf50d20e4300eaf6556dd8f0b581994a5b0e442c
YooranKim <yuranm@naver.com>
2023-12-07 20:59:03 -0800

    organize

--- js/global.js
+++ js/global.js
@@ -5,7 +5,13 @@
     menuTrigger.addEventListener('click', (event) => {
       event.currentTarget.classList.toggle('active-1');
       $(dropdownMenu).slideToggle()
-
-      
     });
 
+
+    
+    // $(function(){
+    //   $('#adoption').counto(1100, 3000);
+    //   $('#rescue').counto(2048, 3000);
+    //   $('#client').counto(950, 3000);
+    //   $('#services').counto(410, 3000);
+    // });
